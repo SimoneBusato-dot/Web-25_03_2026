29a1ff752171496260e9b7f3d79d9be4c4c4a934
SimoneBusato-dot <simone.busato@mail.polimi.it>
2026-03-25 17:12:28 +0100

    Add style local and global

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
+  
+<script>
+
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+
+
+<style>
+h1{
+    color:brown;
+
+}
+</style>
