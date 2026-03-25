7db4feb63d2d8da57642a422d9a7f65d618ce4e4
SimoneBusato-dot <simone.busato@mail.polimi.it>
2026-03-25 17:40:55 +0100

    Import

---
+++ src/lib/components/ClickCounters.svelte
@@ -0,0 +1,20 @@
+<script>
+    let count =$state(0)
+  let double = $derived(count * 2);
+</script>
+
+<button onclick={() => count++}>
+    clicks: {count}
+</button>
+<span> doubled is {double}</span>
+
+<style>
+    button{
+    background-color: rgb(224, 224, 224);
+    border-radius: 10px;
+    border: solid 0.5px whitesmoke;
+    padding: 0.5rem;
+    box-shadow: -4px 3px 8px rgb(158, 158, 158);
+    cursor: pointer;
+}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,20 @@
   
 <script>
-
+ 
+  import ClickCounters from "$lib/components/ClickCounters.svelte";
+  import ProgressiveSum from "$lib/components/ProgressiveSum.svelte";
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-
+<ClickCounters />
 
 <style>
 h1{
     color:brown;
 
 }
+
+
 </style>
